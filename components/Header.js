a8e849aa987fe07bd386f84cf80fd4330bd29ae0
Aman Kumar <amankr1619@gmail.com>
2021-08-11 21:18:34 +0530

    Added Header

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,23 @@
 import React from 'react'
-import { useColorMode, Button, Flex } from "@chakra-ui/react"
+import { useColorMode, Button, Flex, Container, Text,Box, Image } from "@chakra-ui/react"
 import { SunIcon, MoonIcon } from '@chakra-ui/icons'
 const Header = () => {
     const { colorMode, toggleColorMode } = useColorMode()
     return (
         <Flex>
-            <Button onClick={toggleColorMode}>{colorMode === "dark" ? <MoonIcon /> : <SunIcon />}</Button>
+            <Image
+                boxSize="60px"
+                objectFit="cover"
+                src="IIITN.png"
+                alt="IIITN"
+                mx={5} mt={3} mb={2}            
+            />
+            <Box w="100%" p={5}>
+                <Text fontSize="3xl">Indian Institute of Information Technology, Nagpur</Text>
+            </Box>
+            <Button onClick={toggleColorMode} w="60px" h="60px" mx={5} mt={3} mb={2} >
+                {colorMode === "dark" ? <MoonIcon /> : <SunIcon />}
+            </Button>
         </Flex>
     )
 }
--- components/Wrapper.js
+++ components/Wrapper.js
@@ -1,37 +1,16 @@
 import React from 'react'
-import { Wrap, WrapItem, Container, Center } from "@chakra-ui/react"
+import { VStack, Wrap, WrapItem, Container, Center } from "@chakra-ui/react"
 
 const Wrapper = () => {
     return (
         <Container>
-            <Wrap spacing="30px">
-                <WrapItem>
-                    <Center w="180px" h="80px" bg="red.200">
-                    Box 1
-                    </Center>
-                </WrapItem>
-                <WrapItem>
-                    <Center w="180px" h="80px" bg="green.200">
-                    Box 2
-                    </Center>
-                </WrapItem>
-                <WrapItem>
-                    <Center w="180px" h="80px" bg="tomato">
-                    Box 3
-                    </Center>
-                </WrapItem>
-                <WrapItem>
-                    <Center w="180px" h="80px" bg="blue.200">
-                    Box 4
-                    </Center>
-                </WrapItem>
-                <WrapItem>
-                    <Center w="180px" h="80px" bg="blackAlpha.500">
-                    Box 5
-                    </Center>
-                </WrapItem>
-            </Wrap>
-            </Container>
+            <VStack>
+                <Container maxW="container.xl">Extra-Large Container</Container>
+                <Container maxW="container.lg">Large Container</Container>
+                <Container maxW="container.md">Medium Container</Container>
+                <Container maxW="container.sm">Small Container</Container>
+            </VStack>
+        </Container>
     )
 }
 
