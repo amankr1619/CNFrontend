b7a4ebd3c0fa023d03e7a305498f22848202e1c2
Aman Kumar <amankr1619@gmail.com>
2021-08-11 22:42:33 +0530

    Added Server Card

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { useColorMode, Button, Flex, Container, Text,Box, Image } from "@chakra-ui/react"
+import { useColorMode, Button, Flex, Container, Text,Center, Image } from "@chakra-ui/react"
 import { SunIcon, MoonIcon } from '@chakra-ui/icons'
 const Header = () => {
     const { colorMode, toggleColorMode } = useColorMode()
     return (
-        <Flex>
+        <Flex bg = "red">
             <Image
                 boxSize="60px"
                 objectFit="cover"
@@ -12,9 +12,9 @@ const Header = () => {
                 alt="IIITN"
                 mx={5} mt={3} mb={2}            
             />
-            <Box w="100%" p={5}>
+            <Center w="100%" p={5}>
                 <Text fontSize="3xl">Indian Institute of Information Technology, Nagpur</Text>
-            </Box>
+            </Center>
             <Button onClick={toggleColorMode} w="60px" h="60px" mx={5} mt={3} mb={2} >
                 {colorMode === "dark" ? <MoonIcon /> : <SunIcon />}
             </Button>
