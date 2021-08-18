057400f68fa768813646d0aa73a4f3a45b4a1001
Aman Kumar <amankr1619@gmail.com>
2021-08-18 17:20:52 +0530

    Added Backend Handling

--- utils/handleAPI.js
+++ utils/handleAPI.js
@@ -2,21 +2,14 @@
 import axios from "axios"
 
 export const connectionRequest = async ({proto}) => {
-  console.log(proto)
-  try {
-    axios.get('http://127.0.0.1:5000/TCP',
-    {
-      headers: {
-        'Access-Control-Allow-Origin': 'http://127.0.0.1:5000',
-        'Content-Type': 'application/json',
-      },
-    }).then((response) => {
-        console.log(`Client Address is ${response.data.clientAddress} and message from the client is ${response.data.messageFromClient}`);
-      }, (error) => {
-        console.log(error);
-      });
-} catch (error) {
-return error
+    console.log(proto)
+    try {
+      const res = await axios.get(`http://127.0.0.1:5000/${proto}`)
+      console.log(res.data)
+      console.log(`Client Address is ${res.data.clientAddress[0]}/${res.data.clientAddress[1]} and message from the client is ${res.data.messageFromClient}`)
+      return res.data;
 
-}
+  } catch (error) {
+      return error
+  }
 }
