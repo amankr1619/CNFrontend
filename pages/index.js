93debce76328c8b3e9e2271fe45cb9c459f61a1d
Gaurav Bhandarkar <62330601+DracoCoder@users.noreply.github.com>
2021-08-26 15:26:01 +0530

    Loren ipsum fixed

--- pages/index.js
+++ pages/index.js
@@ -121,13 +121,7 @@ export default function Home() {
                   textColor="grey.700"
                 >
                   <Text fontSize='18' color={useColorModeValue('white', 'gray.800')} textAlign='center' >
-                    Lorem ipsum dolor sit amet,
-                    consectetur adipiscing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident,
-                    sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    A network socket is a software structure within a network node of a computer network that serves as an endpoint for sending and receiving data across the network. We Implemented API and Frontend using React.js .
                   </Text>
                 </Box>
               </Stack>
